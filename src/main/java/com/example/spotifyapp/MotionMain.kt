9360fbd92086ad6d1f89582844cc08fb04a57979
traduongdatnguyen <tddnguyen.20it8@vku.udn.vn>
2022-11-01 19:25:47 +0700

    motion layout

---
+++ src/main/java/com/example/spotifyapp/MotionMain.kt
@@ -0,0 +1,11 @@
+package com.example.effecttoolbar
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MotionMain : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_motion_main)
+    }
+}
